43188adbaf95d9032becc8e30d1359f536ca6015
Gustavo Santos <gugahh.br@gmail.com>
2021-07-04 23:07:08 -0300

    Filedownload: melhoria nas instruções

--- filedownload.js
+++ filedownload.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { networkInterfaces } = require('os');
 
 const FILE_PATH = './arquivos/'; 
 const FILE_DOWNLOAD_PORT = 9500;
 
 console.log(">> Iniciando app filedownload. ");
+console.log(">> Baixe seu arquivo em: http://" 
+    + get_local_ip()
+    + ":"
+    + FILE_DOWNLOAD_PORT
+    + "/{nome_do_arquivo}"
+    );
 
 app.get('/about', 
     function (req, res, next) {
@@ -32,5 +39,19 @@ app.get('/download/:filename',
         //res.send(req.params);
     }
 );
+
+function get_local_ip() {
+    const nets = networkInterfaces();
+
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return '<host desconhecido>';
+}
 
 app.listen(FILE_DOWNLOAD_PORT);
